e0a71b8ed2674b49351db3d5134db18278ebaa6e
Juanma <72887157+juanmalescano@users.noreply.github.com>
2022-09-22 21:59:09 -0300

    Update make.sh

--- make.sh
+++ make.sh
@@ -3,12 +3,14 @@ mvn clean && mvn package -DskipTests
 docker build . -t eureka-server
 cd ..
 
-cd server-config/
-sh make.sh
+cd config-service/
+mvn clean && mvn package -DskipTests
+docker build . -t config-service
 cd ..
 
 cd movie-service/
-sh make.sh
+mvn clean && mvn package -DskipTests
+docker build . -t movie-service
 cd ..
 
 cd catalog-service/
@@ -16,12 +18,15 @@ mvn clean && mvn package -DskipTests
 docker build . -t catalog-service
 cd ..
 
-cd api-gateway/
-sh make.sh
+cd series-service/
+mvn clean && mvn package -DskipTests
+docker build . -t series-service
 cd ..
 
-cd series/
-sh make.sh
+cd api-gateway/
+mvn clean && mvn package -DskipTests
+docker build . -t api-gateway
 cd ..
 
 docker-compose up
+
